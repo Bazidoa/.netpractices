ee6493a08d23df891561889e9dea5515aad5c3f8
Balazs Gyongyosi <balzs.gyngysi@gmail.com>
2019-03-13 13:53:06 +0100

    all

---
+++ Week01/01_dotNET alapozás/IntroToVS/IntroToVS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToVS
+{
+    // Ha cmd-ből meghívod akkor kiírja a paraméterek számát és a paramétereket.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Hello Yello!");
+
+        Console.WriteLine(args.Length);
+        for (int i = 0; i < args.Length; i++)
+        {
+            Console.WriteLine();
+        }
+        //Kiírja a megnyomott billentyűt
+            Console.ReadKey();
+        }
+    }
+}
