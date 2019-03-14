9247be2c6eab628c5e67b1191ac69a8f134f0792
Balazs Gyongyosi <balzs.gyngysi@gmail.com>
2019-03-14 10:54:35 +0100

    literals practice

---
+++ Week01/C# nyelvi alapok/Literals/Literals/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Literals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double quotient = 3 / 4d;
+            Console.WriteLine(quotient);
+            // d suffix nélkül két intet oszt el egymással és az valóban 0. így viszont double eredményt ad 
+
+            string imageFile = @"C:\\yellowroad\\images\\yellowroadlogo.png";
+            string emailBody = @"Szia!\nMegkaptuk a jelentkezésedet.\nÜdv,\nYellowroad csapata";
+
+            Console.WriteLine(imageFile);
+            Console.WriteLine(emailBody);
+        }
+    }
+}
