b81532d41e5200c7dd00b1481fd0a15025719af6
Balazs Gyongyosi <balzs.gyngysi@gmail.com>
2019-03-19 15:31:32 +0100

    unit tests

---
+++ Week02/Unit testing/UnitTesting/myUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("myUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("myUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("905b2c94-f27e-4535-8726-284b1826e194")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Week02/Unit testing/UnitTesting/myUnitTest/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTesting;
+
+namespace myUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestHello()
+        {
+            //Arrange
+            Gentleman gentlemanToTest = new Gentleman();
+
+            string excpectedResult = "Hello Billy Mack";
+
+            //Act
+            string actualResult = gentlemanToTest.SayHello("Billy Mack");
+
+
+
+            //Assert
+            Assert.AreEqual(excpectedResult, actualResult);
+
+        }
+    }
+}
