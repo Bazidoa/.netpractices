98e17d783c259f37083bd21b063804d7b8f7294d
Balazs Gyongyosi <balzs.gyngysi@gmail.com>
2019-03-20 11:32:02 +0100

    debugging

---
+++ Week02/Debuggoljunk/Debugging/Debugging/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator myAdder = new Calculator();
+            myAdder.Add(10, 20);
+        }
+    }
+}
