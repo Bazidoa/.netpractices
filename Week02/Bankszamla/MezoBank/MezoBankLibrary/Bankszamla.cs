3a2387ac1b4953eb12027bec22ab5ecec9efb257
Balazs Gyongyosi <balzs.gyngysi@gmail.com>
2019-03-21 17:32:46 +0100

    bankszámla

---
+++ Week02/Bankszamla/MezoBank/MezoBankLibrary/Bankszamla.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MezoBankLibrary
+{
+    public class Bankszamla
+    {
+        private string tulajNev;
+        
+        private int szamlaszam;
+        public int Szamlaszam //5 számjegyű számnak kell lennie
+        {
+            get
+            {
+                return szamlaszam;
+            }
+            set
+            {
+                if (value>9999 && value < 100000 )
+                {
+                szamlaszam = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Nem 5 jegyű számot adtál meg");
+                }
+            }
+        }
+
+        private int egyenleg;
+
+        public Bankszamla(string tulajNev)
+        {
+            this.tulajNev = tulajNev;
+            this.egyenleg = 0;
+            this.szamlaszam = -99;
+        }
+
+        public int EgyenlegNovel (int betettOsszeg)
+        {
+            NemLehetNegativValidalo(betettOsszeg);
+            this.egyenleg += betettOsszeg;
+            return this.egyenleg;
+        }
+
+        public int EgyenlegCsokkent (int kivettOsszeg)
+        {
+            NemLehetNegativValidalo(kivettOsszeg);
+            this.egyenleg -= kivettOsszeg;
+            return this.egyenleg;
+        }
+
+        public void NemLehetNegativValidalo(int beutottOsszeg)
+        {
+            if (beutottOsszeg < 0)
+            {
+                throw new ArgumentOutOfRangeException("Nem lehet negatív a beütött összeg.");
+            }
+        }
+    }
+}
+
---
+++ Week02/Bankszamla/MezoBank/MezoBankUnitTest/BankszamlaTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using MezoBankLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MezoBankUnitTest
+{
+    [TestClass] //attribútum 
+    public class BankszamlaTest
+    {
+        [TestMethod]
+        public void EgyenlegNoveles()
+        {
+            //Arrange
+            int expectedEgyenleg = 120;
+            Bankszamla szla = new Bankszamla("tesztBankszamla");
+            //Act
+
+            int actualEgyenleg = szla.EgyenlegNovel(120);
+            //Assert
+            Assert.AreEqual(expectedEgyenleg, actualEgyenleg);
+        }
+        [TestMethod]
+        public void EgyenlegCsokkentes()
+        {
+            //Arrange
+            Bankszamla szla2 = new Bankszamla("teszt2Bankszamla");
+            int expectedResult = -100;
+
+            //Act
+            int actualResult = szla2.EgyenlegCsokkent(100);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void NemLehetNullaTeszt()
+        {
+            //Arrange
+            Bankszamla szla3 = new Bankszamla("teszbankszamla3");
+
+            //Act
+            szla3.NemLehetNegativValidalo(-10);
+
+            //Assert - expected result -> exception, assert not needed.
+        }
+
+        [TestMethod]
+        public void SzamlaszamSetValidator()
+        {
+            //Arrange
+            Bankszamla szla4 = new Bankszamla("teszt4Bankszamla");
+
+            int excpectedSzlaszam = 12345;
+
+            //Act
+            int actualSzlaszam = (szla4.Szamlaszam = 12345);
+
+            //Assert
+            Assert.AreEqual(excpectedSzlaszam, actualSzlaszam);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void SzamlaszamSetValidatorNotGood()
+        {
+            //Arrange
+            Bankszamla szla5 = new Bankszamla("teszt5bankszamla");
+
+            //Act
+            szla5.Szamlaszam = 123;
+
+
+        }
+
+    }
+}
