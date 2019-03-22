b183e0e52cb6d36f5f83961fa21765cc8587c1b7
Balazs Gyongyosi <balzs.gyngysi@gmail.com>
2019-03-22 20:32:35 +0100

    greetings app + unit test

---
+++ Week02/Selection/KoszonoApp/KoszonoApp/Koszono.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoszonoApp
+{
+    public class Koszono
+    {
+        public string SaySomeKindOfGreeting(int hour, int minutes)
+        {
+            string result ="";
+
+            if (hour > 5)
+            {
+                result = "Jó reggelt!";
+            }
+            if (hour > 9)
+            {
+                result = "Jó napot!";
+            }
+            if (hour >= 18 && minutes > 30)
+            {
+                result = "Jó estét";
+            }
+            if (hour > 20)
+            {
+                result = "Jó éjt";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Week02/Selection/KoszonoApp/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using KoszonoApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void KoszonoMethodReggel()
+        {
+
+            Koszono selectionEx = new Koszono();
+
+            Assert.AreEqual("Jó reggelt!", selectionEx.SaySomeKindOfGreeting(9, 01));
+           
+
+        }
+
+        [TestMethod]
+        public void KoszonoMethodNap()
+        {
+
+            Koszono selectionEx = new Koszono();
+
+            Assert.AreEqual("Jó napot!", selectionEx.SaySomeKindOfGreeting(18, 29));
+
+        }
+        [TestMethod]
+        public void KoszonoMethodEste()
+        {
+
+            Koszono selectionEx = new Koszono();
+
+          
+            Assert.AreEqual("Jó estét", selectionEx.SaySomeKindOfGreeting(18, 31));
+            //Assert.AreEqual("Jó estét", selectionEx.SaySomeKindOfGreeting(19, 59));
+
+        }
+        [TestMethod]
+        public void KoszonoMethodEjjel()
+        {
+
+            Koszono selectionEx = new Koszono();
+
+            Assert.AreEqual("Jó éjt", selectionEx.SaySomeKindOfGreeting(23, 34));
+            //Assert.AreEqual("Jó éjt", selectionEx.SaySomeKindOfGreeting(1, 23));
+           
+        }
+    }
+    
+}
