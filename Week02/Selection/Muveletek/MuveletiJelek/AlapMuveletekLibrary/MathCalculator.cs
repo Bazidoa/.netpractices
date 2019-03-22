3049358f7056b1281effacf5d4bdf7d21a954c7a
Balazs Gyongyosi <balzs.gyngysi@gmail.com>
2019-03-22 20:47:10 +0100

    muveletek + UT

---
+++ Week02/Selection/Muveletek/MuveletiJelek/AlapMuveletekLibrary/MathCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlapMuveletekLibrary
+{
+    public class MathCalculator
+    {
+        public double Calculate(double num1, double num2, string muveletiJel)
+        {
+            double result = 0;
+
+            switch(muveletiJel){
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+            }
+
+            return result;
+
+        }
+    }
+}
---
+++ Week02/Selection/Muveletek/MuveletiJelek/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using AlapMuveletekLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CalculateTestAdd()
+        {
+            MathCalculator testCalc = new MathCalculator();
+            
+            double expectedResult = 7;
+
+            double actualResult = testCalc.Calculate(5, 2, "+");
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void CalculateTestSubs()
+        {
+            MathCalculator testCalc = new MathCalculator();
+
+            double expectedResult = 3;
+
+            double actualResult = testCalc.Calculate(5, 2, "-");
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void CalculateTestTimes()
+        {
+            MathCalculator testCalc = new MathCalculator();
+
+            double expectedResult = 10;
+
+            double actualResult = testCalc.Calculate(5, 2, "*");
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void CalculateTestDevide()
+        {
+            MathCalculator testCalc = new MathCalculator();
+
+            double expectedResult = 2.5;
+
+            double actualResult = testCalc.Calculate(5, 2, "/");
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
