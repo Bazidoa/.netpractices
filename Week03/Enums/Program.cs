54cce1d2ad0eb28c0f1a9c9820eaee671ba56d4c
Balazs Gyongyosi <balzs.gyngysi@gmail.com>
2019-04-04 15:13:56 +0200

    my first enum

---
+++ Week03/Enums/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    public enum Regions : byte { North=1, East, South, West };
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(  Enum.GetValues(typeof(Regions))); //típusát kiírja a Regions-nak
+            foreach (var item in Enum.GetValues(typeof(Regions)))
+            {
+                Console.WriteLine((byte)item);
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+            
+        }
+    }
+
+}
